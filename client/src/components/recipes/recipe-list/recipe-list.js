b42a2b9db6d18596ea39d7230839bfcf5aeee018
JBarrett991 <60449860+JBarrett991@users.noreply.github.com>
2023-01-02 15:49:59 -0500

    frontend

--- client/src/components/recipes/recipe-list/recipe-list.js
+++ client/src/components/recipes/recipe-list/recipe-list.js
@@ -1,3 +1,30 @@
-const RecipeList = () => {
-    return <div className="recipe-list"></div>
-}
+import { Link, Navigate } from "react-router-dom";
+
+const RecipeList = (props) => {
+  const { user } = props;
+  const recipes = [];
+  return (
+    <>
+      {user ? (
+        <div className="recipe-list">
+          {recipes.length > 0 ? (
+            recipes.map(() => (
+              <table>
+                <tr></tr>
+              </table>
+            ))
+          ) : (
+            <>
+              <p>You haven't added any recipes yet</p>
+              <Link>Add a recipe?</Link>
+            </>
+          )}
+        </div>
+      ) : (
+        <Navigate to="/user/login" />
+      )}
+    </>
+  );
+};
+
+export default RecipeList;
--- client/src/components/recipes/recipes.js
+++ client/src/components/recipes/recipes.js
@@ -1,9 +1,11 @@
+import RecipeList from "./recipe-list/recipe-list";
 import RecipeNav from "./recipe-nav/recipe-nav";
 
 const Recipes = () => {
   return (
     <div className="recipes">
       <RecipeNav />
+      <RecipeList />
     </div>
   );
 };
