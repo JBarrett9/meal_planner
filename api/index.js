274867f71358f74d177632560f3ac2c8d9ea41a8
James Barrett <james.barrett991@gmail.com>
2023-03-07 16:41:35 -0500

    passport

--- api/index.js
+++ api/index.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const router = express.Router();
 const jwt = require("jsonwebtoken");
-const passportSetup = require("./passport");
 const passport = require("passport");
 const { getUserById } = require("../db/users");
 
