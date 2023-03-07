75cf439bc3b1b968ab639cfd78a8dadb708a876e
James Barrett <james.barrett991@gmail.com>
2023-03-07 17:22:09 -0500

    passport

--- api/index.js
+++ api/index.js
@@ -2,6 +2,8 @@ const express = require("express");
 const router = express.Router();
 const jwt = require("jsonwebtoken");
 const passport = require("passport");
+const session = require("express-session");
+const pgSession = require("connect-pg-simple")(session);
 const { getUserById } = require("../db/users");
 
 const { JWT_SECRET } = process.env;
@@ -9,6 +11,37 @@ router.use(express.json());
 
 router.use(passport.initialize());
 router.use(passport.session());
+
+const sessionConfig = {
+  store: new pgSession({
+    pool: client,
+    tableName: "session",
+  }),
+  name: "SID",
+  secret: randomString.generate({
+    length: 14,
+    charset: "alphanumeric",
+  }),
+  resave: false,
+  saveUninitialized: true,
+  cookie: {
+    maxAge: 1000 * 60 * 60 * 24 * 7,
+    aameSite: true,
+    secure: false, // ENABLE ONLY ON HTTPS
+  },
+};
+
+app.use(session(sessionConfig));
+
+app.use(
+  session({
+    secret: "keyboard cat",
+    resave: false,
+    saveUninitialized: false,
+    store: new SQLiteStore({ db: "sessions.db", dir: "./var/db" }),
+  })
+);
+app.use(passport.authenticate("session"));
 
 router.use(async (req, res, next) => {
   const prefix = "Bearer ";
@@ -51,6 +84,7 @@ const listsRouter = require("./lists");
 const mealsRouter = require("./meals");
 const recipesRouter = require("./recipes");
 const usersRouter = require("./users");
+const client = require("../db/client");
 
 router.use("/categories", categoriesRouter);
 router.use("/ingredients", ingredientsRouter);
